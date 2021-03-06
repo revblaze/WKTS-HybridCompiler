02f6435f442723d1cd5b28ba7b2cddb155e7ca46
Justin Bush <revblaze@users.noreply.github.com>
2021-03-06 14:55:23 -0500

    Create macOS project

---
+++ macOS/HybridCompiler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HybridCompiler
+//
+//  Created by Justin Bush on 2021-03-06.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
